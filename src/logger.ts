b1a0237615e1ce43660d299bff40524b09bbea8e
Kuvam Bhardwaj <52134060+kuvamdazeus@users.noreply.github.com>
2022-03-06 13:01:36 +0530

    `logger.ts` now shows meaningful dates instead of number of milliseconds passed since 1 Jan 1970

--- src/logger.ts
+++ src/logger.ts
@@ -1,8 +1,11 @@
 import { NextFunction, Request, Response } from "express";
 
 export default function handler(req: Request, _: Response, next: NextFunction) {
+  let date = new Date().toString();
+  date = date.slice(4, date.indexOf("GMT")).trim();
+
   console.log(
-    `\n-> [${Date.now()}] ${req.method} ${req.url}${
+    `\n-> [${date}] ${req.method} ${req.url}${
       req.method === "POST" ? ":\nRequest Body - " + JSON.stringify(req.body) : ""
     }`
   );
